840dfc036caea9fce141a7f84ac7807db94f7679
Ghont <giohan.puentes.hernandez@unillanos.edu.co>
2026-01-06 14:39:46 -0500

    Fix cache error

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,9 @@ export default createMiddleware({
 
   // Used when no locale matches
   defaultLocale: "es",
+
+  // Don't use a prefix for the default locale
+  localePrefix: "as-needed",
 });
 
 export const config = {
--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -7,6 +7,9 @@ export const routing = defineRouting({
 
   // Used when no locale matches
   defaultLocale: "es",
+
+  // Don't use a prefix for the default locale
+  localePrefix: "as-needed",
 });
 
 // Create navigation hooks and utilities
